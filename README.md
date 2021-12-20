59ade2af6b943a18e2c5dfe35924c16591abed74
naseif <d2mds21@gmail.com>
2021-12-20 09:50:07 +0000

    Added README, fixed startServer, added types for the endpoint method. Updated the docs

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # API-Tools-TS
 
-Simply create a RESTful API with your desired endpoints!
+A light-weight library to help you create APIs on the fly using express.js written in TypeScript.
+
+# Motivation
+
+Creating RESTful APIs is not always the easiest, especially when you have a lot of endpoints and functions for each of this endpoints. API-Tools-TS (the name will be changed LOL) provides an easy way to deal with express.js for beginners in more structured way.
+
+# Installation
+
+```
+npm i api-tools-ts
+```
+
+# Importing
+
+## TypeScript
+
+```ts
+import { APIController } from "api-tools-ts";
+
+const api = new APIController(endpoint); // the main endpoint for the api you wish to create
+```
+
+## JavaScript
+
+```js
+const { APIController } = require("api-tools-ts");
+const api = new APIController(endpoint); // the main endpoint for the api you wish to create
+```
+
+# Example
+
+```ts
+import { APIController } from "api-tools-ts";
+
+const api = new APIController("/api/v1");
+
+api.AddEndPoint("/", "get", (req, res) => {
+  res.status(200).json({ home: "home is now accessible" });
+});
+api.AddEndPoint("/:random", "get", (req, res) => {
+  const random = req.params.random;
+  res.status(200).json({ test: Math.round(Math.random() * Number(random)) });
+});
+
+api.startServer("true"); // to apply default middlewares ["cors", "morgan", "helmet"]
+```
--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
@@ -20,12 +20,12 @@ export class APIController {
    * Express Object
    */
 
-  private app: any
+  private app: express.Application
 
   /**
    * Express Router
    */
-  
+
   private router: any
 
   /**
@@ -33,7 +33,7 @@ export class APIController {
    * @param {string} endpoint 
    */
 
-  constructor(endpoint: string ) {
+  constructor(endpoint: string) {
     if (!endpoint) throw new Error("APIController can not be initialized without a main endoint!")
     this.mainEndPoint = endpoint
     this.endpoints = new Map();
@@ -59,24 +59,44 @@ export class APIController {
    * @param {string} endpoint 
    * @param {string} method 
    * @param {function} callback 
+   * 
+   * Example:
+   * ```ts
+   * import { APIController } from "./dist"
+   *
+   * const api = new APIController("/api/v1");
+   *
+   * api.AddEndPoint("/", "get", (req, res) => {
+   * res.status(200).json({ home: "home is now accessible" })
+   * })
+   * 
+   * api.StartServer("true") // to apply default middlewares ["cors", "morgan", "helmet"]
+   * ```
+   * 
    */
 
-  AddEndPoint(endpoint: string, method: string, callback: (req: any, res: any) => void) {
-      this.endpoints.set(endpoint, callback)
-      this.router.route(endpoint)[method](callback)
+  AddEndPoint(endpoint: string, method: "get" | "post" | "put" | "delete" | "patch", callback: (req: Request, res: Response) => void) {
+    this.endpoints.set(endpoint, callback)
+    this.router.route(endpoint)[method](callback)
   }
 
 
   /**
-   * Starts the Server!
+   * Starts the express server 
+   * @param {string} applyDefaultMiddleWares "true" || "false"
    */
 
-  StartServer() {
-    this.CreateMainEndPoint()
+  startServer(applyDefaultMiddleWares?: "true" | "false") {
+    if (applyDefaultMiddleWares && applyDefaultMiddleWares === "true") {
+      this.CreateMainEndPoint()
+    } else {
+      this.app.use(express.json());
+      this.app.use(this.mainEndPoint, this.router)
+    }
     const port = process.env.PORT || 3000;
     this.app.listen(port, () => {
-    console.log(`API is running on localhost:${port}/${this.mainEndPoint}`);
-   });
+      console.log(`API is running on https://localhost:${port}${this.mainEndPoint}`);
+    });
   }
 
 }
